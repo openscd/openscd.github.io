c35923e1423c12e283b6c085e42e048598d8c6f6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-27 12:33:08 +0000

    Deploy openscd/openscd.github.io to github.com/openscd/openscd.github.io.git:master

---
+++ out-tsc/test/unit/mock-editor.js
@@ -0,0 +1,10 @@
+import { __decorate } from "../../../web_modules/tslib.js";
+import { LitElement, customElement } from '../../../web_modules/lit-element.js';
+import { Editing } from '../../src/Editing.js';
+let MockEditor = class MockEditor extends Editing(LitElement) {
+};
+MockEditor = __decorate([
+    customElement('mock-editor')
+], MockEditor);
+export { MockEditor };
+//# sourceMappingURL=mock-editor.js.map
---
+++ out-tsc/test/unit/mock-setter.js
@@ -0,0 +1,10 @@
+import { __decorate } from "../../../web_modules/tslib.js";
+import { LitElement, customElement } from '../../../web_modules/lit-element.js';
+import { Setting } from '../../src/Setting.js';
+let MockSetter = class MockSetter extends Setting(LitElement) {
+};
+MockSetter = __decorate([
+    customElement('mock-setter')
+], MockSetter);
+export { MockSetter };
+//# sourceMappingURL=mock-setter.js.map
