d61794753b1ecb5792220bb427a61ff2ca8d7293
Christian Dinkel (via Travis CI) <christian.dinkel@omicronenergy.com>
2020-10-29 21:09:58 +0000

    Deploy openscd/open-scd to github.com/openscd/openscd.github.io.git:master

--- out-tsc/src/plugin.js
+++
@@ -1,7 +0,0 @@
-export async function plugin(src, tagName) {
-    if (customElements.get(tagName) === undefined) {
-        const mod = await import(src);
-        customElements.define(tagName, mod.default);
-    }
-}
-//# sourceMappingURL=plugin.js.map
