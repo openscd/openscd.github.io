5d3076281ebc1748cbe4bbf0cfe85bf4686b901a
dlabordus <d.labordus@gmail.com>
2022-07-13 14:52:49 +0000

    Deploying to master from @ openscd/open-scd@7a12d77447fcf2f24a8b7f3e2549ce1626f41774 🚀

--- src/editors/ied/da-container.js
+++ src/editors/ied/da-container.js
@@ -143,11 +143,10 @@ DAContainer.styles = css`
       color: var(--mdc-theme-on-surface);
       font-family: 'Roboto', sans-serif;
       font-weight: 300;
-      overflow: hidden;
-      white-space: nowrap;
-      text-overflow: ellipsis;
       margin: 0px;
       padding-left: 0.3em;
+      word-break: break-word; 
+      white-space: pre-wrap;
     }
 
     mwc-icon-button {
