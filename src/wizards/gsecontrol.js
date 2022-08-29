d49423dbf9ade0082123ac1f214d5fb9983023ed
dlabordus <d.labordus@gmail.com>
2022-08-29 05:34:25 +0000

    Deploying to master from @ openscd/open-scd@cc48b0a707cb83a3a3e2473fc8e9b654b8689044 🚀

--- src/wizards/gsecontrol.js
+++ src/wizards/gsecontrol.js
@@ -89,6 +89,7 @@ export function contentGseControlWizard(content) {
 }
 function createGseControlAction(parent) {
   return (inputs, wizard) => {
+    const actions = [];
     const gseControlAttrs = {};
     const gseControlKeys = [
       "name",
@@ -105,63 +106,55 @@ function createGseControlAction(parent) {
     const dataSetName = gseControlAttrs.name + "sDataSet";
     gseControlAttrs["datSet"] = dataSetName;
     const gseControl = createElement(parent.ownerDocument, "GSEControl", gseControlAttrs);
-    let gse = null;
-    let gseParent = null;
+    actions.push({new: {parent, element: gseControl}});
     if (isAccessPointConnected(parent)) {
+      const gseParent = getConnectedAP(parent);
+      const gse = createElement(parent.ownerDocument, "GSE", {
+        ldInst: parent.closest("LDevice")?.getAttribute("inst") ?? "",
+        cbName: gseControlAttrs["name"]
+      });
+      actions.push({new: {parent: gseParent, element: gse}});
       const instType = wizard.shadowRoot?.querySelector("#instType")?.checked ?? false;
       const gseAttrs = {};
       const gseKeys = ["MAC-Address", "APPID", "VLAN-ID", "VLAN-PRIORITY"];
       gseKeys.forEach((key) => {
         gseAttrs[key] = getValue(inputs.find((i) => i.label === key));
       });
-      const minTime = getValue(inputs.find((i) => i.label === "MinTime"));
-      const maxTime = getValue(inputs.find((i) => i.label === "MaxTime"));
-      gse = createElement(parent.ownerDocument, "GSE", {
-        ldInst: parent.closest("LDevice")?.getAttribute("inst") ?? "",
-        cbName: gseControlAttrs["name"]
-      });
       const address = createAddressElement(gseAttrs, gse, instType);
-      const MinTime = createElement(parent.ownerDocument, "MinTime", {
+      actions.push({new: {parent: gse, element: address}});
+      const minTime = getValue(inputs.find((i) => i.label === "MinTime"));
+      const minTimeElement = createElement(parent.ownerDocument, "MinTime", {
         unit: "s",
         multiplier: "m"
       });
-      MinTime.textContent = minTime;
-      const MaxTime = createElement(parent.ownerDocument, "MaxTime", {
+      minTimeElement.textContent = minTime;
+      actions.push({new: {parent: gse, element: minTimeElement}});
+      const maxTime = getValue(inputs.find((i) => i.label === "MaxTime"));
+      const maxTimeElement = createElement(parent.ownerDocument, "MaxTime", {
         unit: "s",
         multiplier: "m"
       });
-      MaxTime.textContent = maxTime;
-      gse.appendChild(address);
-      gse.appendChild(MaxTime);
-      gse.appendChild(MinTime);
-      gseParent = getConnectedAP(parent);
+      maxTimeElement.textContent = maxTime;
+      actions.push({new: {parent: gse, element: maxTimeElement}});
     }
     const dataSet = createElement(parent.ownerDocument, "DataSet", {
       name: dataSetName
     });
+    actions.push({new: {parent, element: dataSet}});
     const finder = wizard.shadowRoot.querySelector("finder-list");
     const paths = finder?.paths ?? [];
     for (const path of paths) {
       const element = newFCDA(parent, path);
       if (!element)
         continue;
-      dataSet.appendChild(element);
+      actions.push({new: {parent: dataSet, element}});
     }
-    const complexAction = gse ? {
-      title: "Create GSEControl",
-      actions: [
-        {new: {parent, element: gseControl}},
-        {new: {parent: gseParent, element: gse}},
-        {new: {parent, element: dataSet}}
-      ]
-    } : {
-      title: "Create GSEControl",
-      actions: [
-        {new: {parent, element: gseControl}},
-        {new: {parent, element: dataSet}}
-      ]
-    };
-    return [complexAction];
+    return [
+      {
+        title: get("editing.created", {name: "GSEControl"}),
+        actions
+      }
+    ];
   };
 }
 export function createGseControlWizard(ln0OrLn) {
