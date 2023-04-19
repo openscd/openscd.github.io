5ed22e6a015cc667505800fdf94fbbb4aaae9a29
Stef3st <35229971+Stef3st@users.noreply.github.com>
2023-04-19 10:48:37 +0000

    Deploying to master from @ openscd/open-scd@fc6af77ee99635ba0cabfcf07bed8bf0802e2eae 🚀

--- src/editors/substation/line-editor.js
+++ src/editors/substation/line-editor.js
@@ -15,11 +15,13 @@ import {
   html,
   LitElement,
   property,
-  state
+  state,
+  query
 } from "../../../_snowpack/pkg/lit-element.js";
 import {translate} from "../../../_snowpack/pkg/lit-translate.js";
 import "../../../_snowpack/pkg/@material/mwc-icon.js";
 import "../../../_snowpack/pkg/@material/mwc-icon-button.js";
+import "../../../_snowpack/pkg/@material/mwc-menu.js";
 import "./conducting-equipment-editor.js";
 import "./function-editor.js";
 import "./general-equipment-editor.js";
@@ -28,9 +30,15 @@ import {styles} from "./foundation.js";
 import {
   getChildElementsByTagName,
   newWizardEvent,
-  newActionEvent
+  newActionEvent,
+  tags
 } from "../../foundation.js";
-import {wizards} from "../../wizards/wizard-library.js";
+import {emptyWizard, wizards} from "../../wizards/wizard-library.js";
+function childTags(element) {
+  if (!element)
+    return [];
+  return tags[element.tagName].children.filter((child) => wizards[child].create !== emptyWizard);
+}
 export let LineEditor = class extends LitElement {
   constructor() {
     super(...arguments);
@@ -43,6 +51,11 @@ export let LineEditor = class extends LitElement {
   }
   openEditWizard() {
     const wizard = wizards["Line"].edit(this.element);
+    if (wizard)
+      this.dispatchEvent(newWizardEvent(wizard));
+  }
+  openCreateWizard(tagName) {
+    const wizard = wizards[tagName].create(this.element);
     if (wizard)
       this.dispatchEvent(newWizardEvent(wizard));
   }
@@ -72,6 +85,10 @@ export let LineEditor = class extends LitElement {
           ?showfunctions=${this.showfunctions}
         ></function-editor>`)}`;
   }
+  updated() {
+    if (this.addMenu && this.addButton)
+      this.addMenu.anchor = this.addButton;
+  }
   remove() {
     if (this.element.parentElement)
       this.dispatchEvent(newActionEvent({
@@ -92,6 +109,11 @@ export let LineEditor = class extends LitElement {
               ></l-node-editor>`)}
         </div>` : html``;
   }
+  renderAddButtons() {
+    return childTags(this.element).map((child) => html`<mwc-list-item value="${child}"
+          ><span>${child}</span></mwc-list-item
+        >`);
+  }
   render() {
     return html`<action-pane label=${this.header}>
       <abbr slot="action" title="${translate("edit")}">
@@ -104,7 +126,26 @@ export let LineEditor = class extends LitElement {
         <mwc-icon-button
           icon="delete"
           @click=${() => this.remove()}
-        ></mwc-icon-button> </abbr
+        ></mwc-icon-button>
+      </abbr>
+      <abbr
+        slot="action"
+        style="position:relative;"
+        title="${translate("add")}"
+      >
+        <mwc-icon-button
+          icon="playlist_add"
+          @click=${() => this.addMenu.open = true}
+        ></mwc-icon-button
+        ><mwc-menu
+          corner="BOTTOM_RIGHT"
+          menuCorner="END"
+          @action=${(e) => {
+      const tagName = e.target.selected.value;
+      this.openCreateWizard(tagName);
+    }}
+          >${this.renderAddButtons()}</mwc-menu
+        ></abbr
       >${this.renderConductingEquipments()}${this.renderGeneralEquipments()}${this.renderFunctions()}${this.renderLNodes()}
     </action-pane>`;
   }
@@ -133,6 +174,12 @@ __decorate([
 __decorate([
   state()
 ], LineEditor.prototype, "header", 1);
+__decorate([
+  query("mwc-menu")
+], LineEditor.prototype, "addMenu", 2);
+__decorate([
+  query('mwc-icon-button[icon="playlist_add"]')
+], LineEditor.prototype, "addButton", 2);
 LineEditor = __decorate([
   customElement("line-editor")
 ], LineEditor);
